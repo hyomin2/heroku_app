5f77c8136fbce382c658daaa0102464898ac49e5
hyomin2 <60458642+hyomin2@users.noreply.github.com>
2020-12-22 14:54:13 +0900

    update 222

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send("hello world"));
+app.get('/', (req, res) => res.send("hello world 222"));
 app.listen(port, () => console.log("Example app listening on port 3000"));
 
